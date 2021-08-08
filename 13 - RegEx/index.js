222fb4d30d695d2c5c5e4e23fb62e4739287bf46
Valter <valteralvees@outlook.com>
2021-08-08 11:51:31 -0300

    Finalização RegEx

--- 13 - RegEx/index.js
+++ 13 - RegEx/index.js
@@ -68,4 +68,35 @@ console.log(padrao.test("Abacaxi"))
 console.log(padrao.test("Abacai"))
 //true
 
-// 7 - 
+// 7 - Ocorrência precisa
+
+const cep = /\d{5}-\d{3}/;
+//será aceito somente um número com 5 digitos seguido de 3 dígitos.
+
+const tel = /\(\d{2}\)\d{4,5}-\d{4}/;
+
+// 8 - método exec
+const digitos = /\d+/;
+
+console.log(digitos.exec("tem o numero 100 aqui"))
+
+// 9 - método match
+const frase = "o numero 100 esta aqui".match(/\d+/)
+console.log(frase)
+
+//10 - choice pattern
+
+const reg = /\w+: (Matheus|João|Maria)/
+
+// 11 - Prática: validando um domínio
+
+const validaDominio = /[?www.]\w+\.com.br|.com/
+
+// 12 - Prática: validando e-mail
+
+const validaEmail = /\w+@\w+\.\w+/
+
+// 13 - Prática: validando data de nascimento
+
+const validarDataNasc = /[0-31]{2}[/][0-12]{2}[/][1920-2020]{4}/
+
